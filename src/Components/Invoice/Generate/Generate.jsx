9abd537941ec3d22a36ebac05c8a12bd7e56e278
kryweb <dreidemyromain@gmail.com>
2020-03-01 14:40:55 +0100

    Ajout de la création de facture

---
+++ src/Components/Invoice/Generate/Generate.jsx
@@ -0,0 +1,109 @@
+import React, {useState, useEffect} from "react";
+import './Generate.scss';
+import Line from "./Line/Line";
+
+function submitForm(){
+
+    let descriptions = document.querySelectorAll("input[name='description[]']");
+    let unit = document.querySelectorAll("input[name='unit[]']");
+    let unitPrice = document.querySelectorAll("input[name='unit_price[]']");
+    let vatPourcentage = document.querySelectorAll("input[name='vat_pourcentage[]']");
+    let vatEuro = document.querySelectorAll("input[name='vat_euro[]']");
+    let extVat = document.querySelectorAll("input[name='ext_vat[]']");
+    let IntVat = document.querySelectorAll("input[name='int_vat[]']");
+    let formData = [];
+
+    for(let i = 0; i < descriptions.length; i++){
+        formData.push([
+            descriptions[i].value,
+            unit[i].value,
+            unitPrice[i].value,
+            vatPourcentage[i].value,
+            vatEuro[i].value,
+            extVat[i].value,
+            IntVat[i].value,
+        ]);
+    }
+
+    console.log(formData);
+}
+
+function Generate() {
+    const [totalExtVat, setTotalExtVat] = useState(0);
+    const [totalIncVat, setTotalIncVat] = useState(0);
+    const [Lines, setLines] = useState([0]);
+    const [ReactEstDébile, setReactEstDébile] = useState(0);
+
+    const updateTotal = () => {
+        console.log("update...");
+        let extVat = document.querySelectorAll("input[name='ext_vat[]']");
+        let IntVat = document.querySelectorAll("input[name='int_vat[]']");
+
+        let totalExtVat = 0;
+        let totalIntVat = 0;
+
+        for(let i = 0; i < extVat.length; i++){
+            totalExtVat = totalExtVat + parseInt(extVat[i].value);
+            totalIntVat = totalIntVat + parseInt(IntVat[i].value);
+        }
+
+        setTotalExtVat(totalExtVat);
+        setTotalIncVat(totalIntVat);
+    };
+
+    const addLine = () => {
+        let allLines = Lines;
+        allLines.push(0);
+        setLines(allLines);
+        setReactEstDébile(ReactEstDébile+1);
+    }
+
+    return(
+        <div id="Generate">
+
+            <div className="generateInfo">
+                <div>
+                    <h1 contentEditable={true}>Set name</h1>
+                    <p>Date modified : 27/01/2019</p>
+                    <p>Date created : 26/01/2019</p>
+                </div>
+                <div className="btn btn-success" onClick={() => {submitForm(); updateTotal()}}>Save</div>
+            </div>
+
+
+            <form id="form-add-invoice">
+                <div className="Table">
+                    <table id="table-add-invoice">
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Unit</th>
+                                <th>Unit Price</th>
+                                <th>VAT %</th>
+                                <th>VAT €</th>
+                                <th>Ext VAT</th>
+                                <th>Inc VAT</th>
+                            </tr>
+                        </thead>
+                        <tbody id="table-add-invoice-body">
+                            {Lines.map(element => {
+                                return <Line/>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </form>
+
+            <div className="card-total">
+                Total Ext VAR : {totalExtVat} € <br/>
+                Total Inc VAT : {totalIncVat} €
+            </div>
+
+            <div className="addLine">
+                <div className="btn-add-invoice" onClick={() => {addLine()}}>New invoice +</div>
+            </div>
+        </div>
+    )
+}
+
+export default Generate;
---
+++ src/Components/Invoice/Generate/Line/Line.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Line() {
+
+    // Mise à jour des ma valeur int_VAT
+    const update = (e) => {
+        let unit = parseInt(e.target.parentNode.parentNode.childNodes[1].firstChild.value),
+            unitPrice = parseInt(e.target.parentNode.parentNode.childNodes[2].firstChild.value),
+            vatPourcentage = parseInt(e.target.parentNode.parentNode.childNodes[3].firstChild.value),
+            IntVat = 0,
+            ExtVat = 0,
+            vatEuro = 0;
+
+        ExtVat = unit * unitPrice;
+        vatEuro = ExtVat / 100 * vatPourcentage;
+
+        IntVat = ExtVat + vatEuro;
+
+        e.target.parentNode.parentNode.childNodes[4].firstChild.value = vatEuro;
+        e.target.parentNode.parentNode.childNodes[5].firstChild.value = ExtVat;
+        e.target.parentNode.parentNode.childNodes[6].firstChild.value = IntVat;
+    };
+
+    return (
+        <tr>
+            <td><input type="text" name="description[]" placeholder="description..."/></td>
+            <td><input type="number" name="unit[]" placeholder="0" onChange={(e) => {update(e)}}/></td>
+            <td><input type="number" name="unit_price[]" placeholder="0" onChange={(e) => {update(e)}}/></td>
+            <td><input type="number" name="vat_pourcentage[]" placeholder="0" onChange={(e) => {update(e)}}/></td>
+            <td><input type="number" name="vat_euro[]" placeholder="0" defaultValue={0} disabled={true}/></td>
+            <td><input type="number" name="ext_vat[]" placeholder={"0"} defaultValue={0} disabled={true}/></td>
+            <td><input type="number" name="int_vat[]" placeholder="0" defaultValue={0} disabled={true}/></td>
+        </tr>
+    );
+}
+
+export default Line;
